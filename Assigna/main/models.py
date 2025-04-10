8465277fc6d0d3500f0c724c298f88b6bce16b4d
brshkins <wwwdigudkina@gmail.com>
2025-04-11 02:34:48 +0300

    модель для вопросов, ответов и результатов

--- Assigna/main/models.py
+++ Assigna/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.CharField(max_length=255)
+    profession = models.CharField(max_length=255)  # например: 'Программист', 'Дизайнер'
+
+    def __str__(self):
+        return self.text
+
+class UserResult(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    answers = models.ManyToManyField(Answer)
