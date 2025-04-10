f61a65cfd5ff355c9b2eb8a82d41d98af33a8c7e
brshkins <wwwdigudkina@gmail.com>
2025-04-10 22:58:38 +0300

    добавление файлов

---
+++ Assigna/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def test1(request):
+    return render(request, 'main/test1.html')
+
+def test2(request):
+    return render(request, 'main/test2.html')
+
+def test3(request):
+    return render(request, 'main/test3.html')
+
+def itog(request):
+    return render(request, 'main/itog.html')
