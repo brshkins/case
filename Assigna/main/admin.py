fc5f692bf4f3b6851517c73020c17f263046afea
brshkins <wwwdigudkina@gmail.com>
2025-04-11 15:36:00 +0300

    добавлен логотип и иконки

--- Assigna/main/admin.py
+++ Assigna/main/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import Question, Answer, UserResult
-
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(UserResult)
--- Assigna/main/models.py
+++ Assigna/main/models.py
@@ -1,19 +1,1 @@
 from django.db import models
-
-class Question(models.Model):
-    text = models.TextField()
-
-    def __str__(self):
-        return self.text
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    text = models.CharField(max_length=255)
-    profession = models.CharField(max_length=255)  # например: 'Программист', 'Дизайнер'
-
-    def __str__(self):
-        return self.text
-
-class UserResult(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    answers = models.ManyToManyField(Answer)
