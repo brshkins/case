7ed6f23fa64113b372ef3f683d43245da16790f4
brshkins <wwwdigudkina@gmail.com>
2025-04-11 03:31:07 +0300

    отредачены кнопки переключений страниц

--- Assigna/main/urls.py
+++ Assigna/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('test1/', views.test1, name='test1'),
     path('test2/', views.test2, name='test2'),
     path('test3/', views.test3,  name='test3'),
--- Assigna/main/views.py
+++ Assigna/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
+
     return render(request, 'main/index.html')
 
 def test1(request):
