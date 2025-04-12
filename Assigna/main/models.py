86149a35f1646d7408d02be2b296b52d0f5cd8e7
brshkins <wwwdigudkina@gmail.com>
2025-04-12 04:08:31 +0300

    добавлена модель для подключения к бд

--- Assigna/main/models.py
+++ Assigna/main/models.py
@@ -1,1 +1,41 @@
 from django.db import models
+
+#модель для сохранения результатов
+class TestResult(models.Model):
+    name = models.CharField(max_length=100)
+    result = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}: {self.result}"
+
+#модель для профессий
+from django.db import models
+
+
+class Profession(models.Model):
+    # Код RIASEC (например, "SIA", "RIA")
+    code = models.CharField(max_length=10, verbose_name="Код RIASEC")
+
+    # Название профессии
+    profession_name = models.CharField(max_length=255, verbose_name="Название профессии")
+
+    # Основной тип личности (первая буква из RIASEC)
+    main_type = models.CharField(
+        max_length=1,
+        choices=[
+            ('R', 'Realistic'),
+            ('I', 'Investigative'),
+            ('A', 'Artistic'),
+            ('S', 'Social'),
+            ('E', 'Enterprising'),
+            ('C', 'Conventional')
+        ],
+        verbose_name="Основной тип личности",
+        null=True,
+        blank=True
+    )
+
+    # Дополнительная профессия или нет (для третьей буквы)
+    is_additional = models.BooleanField(default=False, verbose_name="Дополнительная профессия")
+
+    def __str__(self):
+        return f"{self.profession_name} ({self.code})"
--- Assigna/main/views.py
+++ Assigna/main/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import TestForm
+from .models import TestResult, Profession
+from django.db.models import Q
 
 def index(request):
-
     return render(request, 'main/index.html')
 
 def test1(request):
-    return render(request, 'main/test1.html')
+    if request.method == 'POST':
+        form = TestForm(request.POST)
+        if form.is_valid():
+            answers = form.cleaned_data
+            # Подсчитаем результат, например, на основе кодов (RIASEC)
+            code = calculate_code(answers)  # Функция для вычисления кода
+            user_name = request.POST.get('user_name', 'Anonymous')
+
+            # Сохраняем результаты в базу данных
+            test_result = TestResult.objects.create(user_name=user_name, answers=answers, code=code)
+
+            # Получаем профессии по коду
+            professions = Profession.objects.filter(Q(code__icontains=code[0]) | Q(code__icontains=code[1]) | Q(code__icontains=code[2]))
+
+            return render(request, 'main/test1_result.html', {'professions': professions, 'user_name': user_name, 'code': code})
+    else:
+        form = TestForm()
+
+    return render(request, 'main/test1.html', {'form': form})
+
+# Функция для вычисления кода RIASEC на основе ответов
+def calculate_code(answers):
+    # Простая логика для примера: выбираем первые буквы ответов
+    code = answers['question_1'][0] + answers['question_2'][0]  # Добавь остальные вопросы
+    return code
 
 def test2(request):
     return render(request, 'main/test2.html')
