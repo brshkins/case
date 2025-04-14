f40909ccf5a42e0e50d2b1eb5a36d92144092663
brshkins <wwwdigudkina@gmail.com>
2025-04-14 20:24:29 +0300

    зарегестрирована модель

--- Assigna/main/admin.py
+++ Assigna/main/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import TestResult, Profession, MBTIResult
+
+admin.site.register(TestResult)
+admin.site.register(Profession)
+admin.site.register(MBTIResult)
