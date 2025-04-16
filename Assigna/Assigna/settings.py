69b11f1516a03c71f0ceed72ea516431acbc21a6
brshkins <wwwdigudkina@gmail.com>
2025-04-16 23:23:58 +0300

    айпишник изменён

--- Assigna/Assigna/settings.py
+++ Assigna/Assigna/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'assignacase.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'assigna-case.onrender.com']
 
 
 # Application definition
